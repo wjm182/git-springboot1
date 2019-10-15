abd050d55544ae7797c4a681fd9a30539178de14
HX <wjm18203542595>
2019-10-15 14:43:01 +0800

    git 第一次commit

---
+++ git-springbootdemo/src/main/java/com/youzhong/GitSpringbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.youzhong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSpringbootdemoApplication.class, args);
+	}
+
+}

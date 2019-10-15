3e353dac4fc89797daa205280ff4d0e21997835e
李娜 <li_linali@163.com>
2019-10-15 20:36:27 +0800

    修改user

--- git-springbootdemo/src/main/java/com/youzhong/entity/user.java
+++ git-springbootdemo/src/main/java/com/youzhong/entity/user.java
@@ -4,8 +4,21 @@ public class user {
 
     private int id;
     private int age;
+    private int name;
+
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
 
     public user(int id) {
         this.id = id;
     }
+
+
+
 }
